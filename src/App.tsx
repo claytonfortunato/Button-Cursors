79a88d599e4f5f896ffda4164e35cb3d95d3bc03
claytonfortunato <claytonreis10@gmail.com>
2023-03-16 01:11:09 -0300

    feat: components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,97 @@
 import * as C from "./styles";
 
+import Button from "./components/Button";
+
+import { useRef, useState } from "react";
+import { buttons, ButtonType } from "./types/buttons";
+
 function App() {
-  return <C.Container></C.Container>;
+  const [list, setList] = useState<ButtonType>(buttons);
+
+  const dragItem = useRef<number>();
+  const dragOverItem = useRef<number>();
+
+  const handleDragStart = (
+    e: React.DragEvent<HTMLButtonElement>,
+    position: number
+  ) => {
+    e.stopPropagation();
+    dragItem.current = position;
+  };
+
+  const handleDragEnter = (
+    e: React.DragEvent<HTMLButtonElement>,
+    position: number
+  ) => {
+    e.stopPropagation();
+    dragOverItem.current = position;
+  };
+
+  const handleDropPartially = (position: number) => {
+    const dragItemCurrent = dragItem.current;
+    const dragOverItemCurrent = dragOverItem.current;
+
+    if (dragItemCurrent === undefined || dragOverItemCurrent === undefined)
+      return;
+
+    if (dragItemCurrent === dragOverItemCurrent) return;
+
+    const newList = [...list];
+    const item = newList[dragItemCurrent];
+    newList.splice(dragItemCurrent, 1);
+    newList.splice(position, 0, item);
+
+    dragItem.current = position;
+    dragOverItem.current = position;
+
+    setList(newList);
+  };
+
+  return (
+    <C.Container>
+      <C.Header>
+        <h1>Teste os botões</h1>
+        <span>
+          Interaja com os botões e observe a mudança de aparência e de cursores
+        </span>
+      </C.Header>
+      <div className="grid">
+        {list.map((item, idx) => (
+          <Button
+            onDragStart={(e) => {
+              e.currentTarget.style.cursor = "grabbing";
+              handleDragStart(e, idx);
+            }}
+            onDragOver={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleDropPartially(idx);
+            }}
+            onDragCapture={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              e.currentTarget.style.cursor = "grabbing";
+              e.currentTarget.style.opacity = "0";
+            }}
+            onDragEnter={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleDragEnter(e, idx);
+            }}
+            onDragEnd={(e) => {
+              handleDropPartially(idx);
+              e.currentTarget.style.cursor = "move";
+              e.currentTarget.style.opacity = "1";
+            }}
+            key={item.id}
+            {...item.props}
+          >
+            {item.label}
+          </Button>
+        ))}
+      </div>
+    </C.Container>
+  );
 }
 
 export default App;
